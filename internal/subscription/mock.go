1c8b8213b36510aeb8e0d6b0444c7b6589c2acff
Omid Tavakoli <omidtavakoli.info@gmail.com>
2021-10-18 14:26:13 +0330

    more mock data generator added

--- internal/subscription/mock.go
+++ internal/subscription/mock.go
@@ -30,11 +30,32 @@ func (s service) PlanGenerator() {
 		s.logger.Errorf("err fetching products:%s", err)
 	} else {
 		for i, product := range products {
-			plan, cpErr := s.psql.CreatePlan("LifeTime", i*1000, 10, -1, product)
+			plan, cpErr := s.psql.CreatePlan("LifeTime", (i+1)*1000, 10, -1, product)
 			if cpErr != nil {
 				s.logger.Errorf("err creating product:%s", cpErr)
 			} else {
 				s.logger.Infof("PlanId:%d created", plan)
+			}
+
+			oneMonthPlan, cpErr := s.psql.CreatePlan("One Month", (i+1)*100, 2, 30, product)
+			if cpErr != nil {
+				s.logger.Errorf("err creating product:%s", cpErr)
+			} else {
+				s.logger.Infof("PlanId:%d created", oneMonthPlan)
+			}
+
+			threeMonthPlan, cpErr := s.psql.CreatePlan("Three Months", (i+1)*300, 6, 90, product)
+			if cpErr != nil {
+				s.logger.Errorf("err creating product:%s", cpErr)
+			} else {
+				s.logger.Infof("PlanId:%d created", threeMonthPlan)
+			}
+
+			sixMonthPlan, cpErr := s.psql.CreatePlan("Six Months", (i+1)*300, 8, 180, product)
+			if cpErr != nil {
+				s.logger.Errorf("err creating product:%s", cpErr)
+			} else {
+				s.logger.Infof("PlanId:%d created", sixMonthPlan)
 			}
 		}
 	}
