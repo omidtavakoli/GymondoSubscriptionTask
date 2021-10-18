9d454af2a2ed2994e1f2186859974b14eb8e8398
Omid Tavakoli <omidtavakoli.info@gmail.com>
2021-10-18 14:25:22 +0330

    schema file updated and tested

--- db/migrations/gymondo_up.sql
+++ db/migrations/gymondo_up.sql
@@ -1,60 +1,68 @@
 CREATE TABLE "users" (
-  "id" SERIAL UNIQUE PRIMARY KEY NOT NULL,
-  "user_name" varchar UNIQUE,
-  "email" varchar UNIQUE NOT NULL,
-  "full_name" varchar,
-  "created_at" timestamp DEFAULT (now()),
-  "updated_at" timestamp DEFAULT (now()),
-  "active_voucher" int
+                         "id" SERIAL UNIQUE PRIMARY KEY NOT NULL,
+                         "user_name" varchar UNIQUE,
+                         "email" varchar UNIQUE NOT NULL,
+                         "full_name" varchar,
+                         "created_at" timestamp DEFAULT (now()),
+                         "updated_at" timestamp DEFAULT (now()),
+                         "deleted_at" timestamp,
+                         "active_voucher" int
 );
 
 CREATE TABLE "products" (
-  "id" SERIAL UNIQUE PRIMARY KEY NOT NULL,
-  "name" varchar,
-  "created_at" timestamp DEFAULT (now()),
-  "updated_at" timestamp DEFAULT (now())
+                            "id" SERIAL UNIQUE PRIMARY KEY NOT NULL,
+                            "name" varchar,
+                            "created_at" timestamp DEFAULT (now()),
+                            "updated_at" timestamp DEFAULT (now()),
+                            "deleted_at" timestamp
 );
 
 CREATE TABLE "plans" (
-  "id" SERIAL UNIQUE PRIMARY KEY NOT NULL,
-  "name" varchar,
-  "price" int,
-  "discount" int,
-  "created_at" timestamp DEFAULT (now()),
-  "updated_at" timestamp DEFAULT (now()),
-  "product_id" int,
-  "duration" int
+                         "id" SERIAL UNIQUE PRIMARY KEY NOT NULL,
+                         "name" varchar,
+                         "price" int,
+                         "discount" int,
+                         "created_at" timestamp DEFAULT (now()),
+                         "updated_at" timestamp DEFAULT (now()),
+                         "deleted_at" timestamp,
+                         "product_id" int,
+                         "duration" int
 );
 
 CREATE TABLE "user_plans" (
-  "id" SERIAL UNIQUE PRIMARY KEY NOT NULL,
-  "user_id" int,
-  "plan_id" int,
-  "plan_status" varchar,
-  "start_date" timestamp DEFAULT (now()),
-  "end_date" timestamp DEFAULT (now()),
-  "voucher" int,
-  "tax" int
+                              "id" SERIAL UNIQUE PRIMARY KEY NOT NULL,
+                              "user_id" int,
+                              "plan_id" int,
+                              "plan_status" varchar,
+                              "start_date" timestamp DEFAULT (now()),
+                              "end_date" timestamp DEFAULT (now()),
+                              "voucher" int,
+                              "tax" int,
+                              "created_at" timestamp DEFAULT (now()),
+                              "updated_at" timestamp DEFAULT (now()),
+                              "deleted_at" timestamp
 );
 
 CREATE TABLE "vouchers" (
-  "id" SERIAL UNIQUE PRIMARY KEY NOT NULL,
-  "name" varchar,
-  "discount" int,
-  "discount_type" varchar,
-  "valid" boolean,
-  "created_at" timestamp DEFAULT (now()),
-  "updated_at" timestamp DEFAULT (now()),
-  "start" datetime,
-  "end" datetime
+                            "id" SERIAL UNIQUE PRIMARY KEY NOT NULL,
+                            "name" varchar,
+                            "discount" int,
+                            "discount_type" varchar,
+                            "valid" boolean,
+                            "created_at" timestamp DEFAULT (now()),
+                            "updated_at" timestamp DEFAULT (now()),
+                            "start" timestamp,
+                            "end" timestamp,
+                            "deleted_at" timestamp
 );
 
 CREATE TABLE "voucher_plan" (
-  "id" SERIAL UNIQUE PRIMARY KEY NOT NULL,
-  "voucher_id" int,
-  "plan_id" int,
-  "created_at" timestamp DEFAULT (now()),
-  "updated_at" timestamp DEFAULT (now())
+                                "id" SERIAL UNIQUE PRIMARY KEY NOT NULL,
+                                "voucher_id" int,
+                                "plan_id" int,
+                                "created_at" timestamp DEFAULT (now()),
+                                "updated_at" timestamp DEFAULT (now()),
+                                "deleted_at" timestamp
 );
 
 ALTER TABLE "users" ADD FOREIGN KEY ("active_voucher") REFERENCES "vouchers" ("id");
